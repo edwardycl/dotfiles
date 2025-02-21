6474c92a40967b01e7d22a53f25476e28ee56438
Youngchan Lee <youngchan1115@gmail.com>
2025-02-21 14:47:03 +0900

    update nvim config

--- coding/nvim/lua/plugins/editor.lua
+++ coding/nvim/lua/plugins/editor.lua
@@ -1,79 +1,33 @@
+local fzf_actions = require("fzf-lua").actions
+
 return {
-  -- TODO: keybindings
-  -- {
-  --   "folke/flash.nvim",
-  --   event = "VeryLazy",
-  --   ---@type Flash.Config
-  --   opts = {
-  --     label = {
-  --       -- show the label after the match
-  --       after = true, ---@type boolean|number[]
-  --       -- show the label before the match
-  --       before = true, ---@type boolean|number[]
-  --       -- Enable this to use rainbow colors to highlight labels
-  --       -- Can be useful for visualizing Treesitter ranges.
-  --       rainbow = {
-  --         enabled = true,
-  --         -- number between 1 and 9
-  --         shade = 5,
-  --       },
-  --     },
-  --     -- You can override the default options for a specific mode.
-  --     -- Use it with `require("flash").jump({mode = "forward"})`
-  --     ---@type table<string, Flash.Config>
-  --     modes = {
-  --       -- options used when flash is activated through
-  --       -- `f`, `F`, `t`, `T`, `;` and `,` motions
-  --       char = {
-  --         enabled = false,
-  --       },
-  --     },
-  --   },
-  --   keys = function()
-  --     return {
-  --       {
-  --         "<C-f>",
-  --         mode = { "n", "o", "x" },
-  --         function()
-  --           require("flash").jump()
-  --         end,
-  --         desc = "Flash",
-  --       },
-  --       {
-  --         "<C-t>",
-  --         mode = { "o", "x" },
-  --         function()
-  --           require("flash").treesitter()
-  --         end,
-  --         desc = "Flash Treesitter",
-  --       },
-  --       {
-  --         "r",
-  --         mode = "o",
-  --         function()
-  --           require("flash").remote()
-  --         end,
-  --         desc = "Remote Flash",
-  --       },
-  --       {
-  --         "R",
-  --         mode = { "o", "x" },
-  --         function()
-  --           require("flash").treesitter_search()
-  --         end,
-  --         desc = "Flash Treesitter Search",
-  --       },
-  --       {
-  --         "<C-s>",
-  --         mode = { "c" },
-  --         function()
-  --           require("flash").toggle()
-  --         end,
-  --         desc = "Toggle Flash Search",
-  --       },
-  --     }
-  --   end,
-  -- },
+  {
+    "ibhagwan/fzf-lua",
+    optional = true,
+    opts = {
+      files = {
+        actions = {
+          ["alt-i"] = false,
+          ["alt-h"] = false,
+          ["ctrl-i"] = { fzf_actions.toggle_ignore },
+          ["ctrl-h"] = { fzf_actions.toggle_hidden },
+        },
+      },
+      grep = {
+        actions = {
+          ["alt-i"] = false,
+          ["alt-h"] = false,
+          ["ctrl-i"] = { fzf_actions.toggle_ignore },
+          ["ctrl-h"] = { fzf_actions.toggle_hidden },
+        },
+      },
+    },
+  },
+  -- TODO: change keybindings
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
   -- Better increment/decrement
   {
     "monaqa/dial.nvim",
